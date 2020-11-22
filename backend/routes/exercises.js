57468484b220b75390d80367d97c11a948276df4
rtwele <ryantwele@gmail.com>
2020-11-21 23:03:42 -0500

    hopefully

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
 
-router.route('/').get((req, res) => {
+router.route('/exercisetracker/').get((req, res) => {
   Exercise.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('Error: ' + err));
