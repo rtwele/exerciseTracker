8a6d00e833803701854e9178fbdb6fff90675f79
rtwele <ryantwele@gmail.com>
2020-11-21 23:14:51 -0500

    originals

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
     <Navbar />
     <br/>
-      <Route path="/exercisetracker/" exact component={ExercisesList} />
+      <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,11 +6,11 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/exercisetracker/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/exercisetracker/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">Exercises</Link>
           </li>
           <li className="navbar-item">
           <Link to="/create" className="nav-link">Create Exercise Log</Link>
